ad25ebba1626f70a2dd40a5388545eaf40398966
maksym <osypchukmaxim.m8@gmail.com>
2021-11-21 11:34:18 +0200

    Change app description

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Мой Git проект.");
+        System.out.println("Мой Git проект. Development");
         System.out.println("Мой Super проект.");
     }
 }
