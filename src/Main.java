e844fc28e52f883623ae89226754ad6cb3bfb890
maksym <osypchukmaxim.m8@gmail.com>
2021-11-21 11:43:27 +0200

    Add sum method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
         System.out.println("Мой Git проект.");
         System.out.println("Мой Super проект.");
+
+        int a = 5;
+        int b = 6;
+
+        int sum = countSum(a, b);
+    }
+    private static int countSum(int a, int b){
+        return a * b;
     }
 }
